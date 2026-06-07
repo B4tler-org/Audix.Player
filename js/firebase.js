92a8e6bb8b5ee2d7e5045cdef441087d7fcf685f
B4tler-org <sameerxettri65@gmail.com>
2026-06-07 15:59:09 +0545

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDN_EWDxVXQiEXB0GV9esg3RZJIRrvMkpg",
+  authDomain: "audix-cf5dd.firebaseapp.com",
+  projectId: "audix-cf5dd",
+  storageBucket: "audix-cf5dd.firebasestorage.app",
+  messagingSenderId: "251879225148",
+  appId: "1:251879225148:web:59de2af7eba8bb631e9039",
+  measurementId: "G-858KTP0X10"
+};
